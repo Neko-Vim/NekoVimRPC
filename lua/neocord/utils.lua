665159671511aeadbb7ac9e759a5d99349c1f6aa
tungstengmd <118553985+tungstengmd@users.noreply.github.com>
2024-07-07 08:55:10 +0100

    Update utils.lua

--- lua/neocord/utils.lua
+++ lua/neocord/utils.lua
@@ -31,7 +31,7 @@ function utils.get_asset_url(asset_name)
 end
 
 function utils.get_logo_url(logo_name)
-  return string.format("https://raw.githubusercontent.com/Neko-Vim/NekoVimRPC/main/assets/logos/%s.png", logo_name)
+  return string.format("https://raw.githubusercontent.com/Neko-Vim/NekoVimRPC/main/assets/logos/NekoVim.png", logo_name)
 end
 
 -- To ensure consistent option values, coalesce true and false values to 1 and 0
